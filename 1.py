77906ca8c033ea3009016541316f533333b5a675
Marshmen4ik <ujl201@bk.ru>
2021-04-15 22:09:46 +0300

    bot is work

--- 1.py
+++ 1.py
@@ -1,16 +1,23 @@
-from discord.ext.commands import Bot
+import discord
+from discord.ext import commands
 
-TOKEN = "ODMyMjg3MjIwNTYzNTc0ODQy.YHhl4A.CWinKRReJe8a-sZgR_JNUFRxpgg"
+settings = {
+    'token': 'ODMyMzI3NjIxMTM1NDMzNzU4.YHiLgA.PpFKpb-b2SjWMDl4vai_MEW65kQ',
+    'bot': 'bot-231412341234',
+    'id': 832327621135433758,
+    'prefix': '$'
+}
 
-client = Bot(description="Test bot", command_prefix="!", pm_help=False)
+bot = commands.Bot(
+    command_prefix=settings['prefix'])  # Так как мы указали префикс в settings, обращаемся к словарю с ключом prefix.
 
 
-@client.event
-async def on_message(msg):
-    if msg.author != client.user:
-        if msg.content.lower() == 'привет':
-            await msg.channel.send(f'Привет, {msg.author.mention}')
+@bot.command()  # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
+async def hello(ctx):  # Создаём функцию и передаём аргумент ctx.
+    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
+
+    await ctx.send(
+        f'Hello, {author.mention}!')  # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
 
 
-async def on_message(msg):
-    client.run(TOKEN)
+bot.run(settings['token'])  # Обращаемся к словарю settings с ключом token, для получения токена
