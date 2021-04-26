adeda5c9f903bc229bbac384b5f35272534536d9
Marshmen4ik <Marshmen4ik>
2021-04-26 21:12:19 +0300

    24

--- commands/admin/add_coins.py
+++ commands/admin/add_coins.py
@@ -3,6 +3,5 @@ from discord_bot.config import cursor, sqlite_connection
 
 def add_coins(msg_user, count_win):  # функция для добавления коинов
     money = cursor.execute(f"""SELECT money FROM members WHERE id_of_user = {msg_user.id}""").fetchall()
-    print(money)
     cursor.execute(f"""UPDATE members SET money = {int(money[0][0]) + count_win} WHERE id_of_user = {msg_user.id}""")
     sqlite_connection.commit()
--- commands/admin/take_away_coins.py
+++ commands/admin/take_away_coins.py
@@ -3,6 +3,5 @@ from discord_bot.config import cursor, sqlite_connection
 
 def take_away_coins(msg_user, count_lose):  # функция для добавления коинов
     money = cursor.execute(f"""SELECT money FROM members WHERE id_of_user = {msg_user.id}""").fetchall()
-    print(money)
     cursor.execute(f"""UPDATE members SET money = {int(money[0][0]) - count_lose} WHERE id_of_user = {msg_user.id}""")
     sqlite_connection.commit()
